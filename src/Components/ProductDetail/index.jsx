7e15bcc0461fa3cc5ef0d971e216ee56de628a8b
Kevin <jefercort@gmail.com>
2023-11-23 15:07:50 -0500

    Primera parte de iconos, instalacion de heroicons e implantacion del primer icono

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,3 +1,4 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
 import "./styles.css"
 
 const ProductDetail = () => {
@@ -6,7 +7,9 @@ const ProductDetail = () => {
         <aside className="product-detail flex flex-col fixed right-0 bg-white border border-black rounded-lg">
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">Detail</h2>
-                <div>X</div>
+                <div>
+                    <BeakerIcon className="h-6 w-6 text-blue-500" />
+                </div>
             </div>
         </aside>
     );
