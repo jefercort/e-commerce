795799b12bb5b2bc27d87f70a0b9c9792d130a21
Kevin <jefercort@gmail.com>
2023-11-23 12:54:56 -0500

    Se hizo una vinculacion a una API para poder llenar las cards con los diferentes datos que la misma contiene

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,20 @@
-const Card = () => {
+// entre parentesis ponemos (data) para indicarle a la card que va a llegar algo y en los datos que cambian indicarle a cada uno que debe cambiar con respecto al tipo de dato que recibe
+const Card = (data) => {
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounde-lg">
             {/* necesitamos que figura tenga elementos absolutos y usamos la propiedad relative & absolute  */}
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</span>
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name}</span>
                 {/* Para encontrar imagenes gratis entramos a la pagina pexels.com 
                 le pusimos las propiedades   para que la foto ocupe todo el ancho y todo el alto de figure pero que tambien no se deforme con esas directivas*/}
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/1037999/pexels-photo-1037999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headphones" />
+                <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.description} />
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white h-6 w-6 rounded-full m-2 p-1">
                     +
                 </div>
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-semibold">$300</span>
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-semibold">${data.data.price}</span>
             </p>
         </div>
     );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,47 @@
+// vamos a importar un custome hook que se llama useState para poder consumir la api
+// siempre que vayamos a consumir una api en nuestro codigo no puede faltar useEffect porque nos ayuda a encapsular esa api (respuesta)
+// pero tambien podemos enviarle los valores iniciales por si necesitamos atraparlo en alguna funcion
+import { useState } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import { useEffect } from "react"
 function Home() {
+    // aca vamos a almacenar lo que viene desde la API .. items es nuestra cajita y setItems es el que va a inyectar el valor de los items
+    // cuando usamos useState() dentro de el podemos poner un valor por defecto que podrian ser unas llaves indicando que en futuro puede ser un objeto
+    // o puede ser un array vacio o un boolean o lo que sea
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+      // para ir a la API y consurmirla usamos el comando fetch y esa informacion viene en tipo promesa, entonces lo que hacemos para resolver la promesa es usar .then y
+      // lo que hacemos es tomar la respuesta "response" y a esa respuesta le decimos que lo haga en JSON
+      fetch("https://api.escuelajs.co/api/v1/products")
+
+      // para ver como aparece la informacion que esta llegandonos usamos el console.log
+        // .then(response => console.log(response.json()))
+
+      // con esta forma podemos ver como se tranforma los datos en arrays como la necesitamos
+        // .then(data => console.log(data))
+        .then(response => response.json())
+        // aca le decimos en donde vamos a almacenar esa DATA y se guarda en el estado y la almacenamos en setItems y le enviamos (data) para visualizarla posteriormente
+        .then(data => setItems(data))
+      // dejamos un array vacio en el segundo item para que sea un valor por defecto pero como no le queremos enviar nada entonces lo dejamos vacio
+      // esto es por si necesitamos enviarle informacion al useEffect por dentro
+    }, [])
+
     return (
       <Layout>
         Home
-        <Card />
+        {/* para que dependiendo de la cantidad de items que querramos me muestre lo que necesito para ponerle logica a lo que quiero renderizar utilizo las llavez {} */}
+        {/* para hacer esto hacemos una triada de condicional preguntando si está y si si esta entonces usamos .map porque es un array para que nos renderice eso */}
+        <div className="grid grid-cols-4 w-full max-w-screen-lg">
+          {
+            items?.map(item => (
+              <Card key={item.id} data={item} />
+            ))
+          }
+        </div>
       </Layout>
     );
   }
-  
+
   export default Home;
