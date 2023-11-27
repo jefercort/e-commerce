8b176d251531313258d1bf2cf9c96ef4ec09da75
Kevin <jefercort@gmail.com>
2023-11-27 10:04:13 -0500

    Se hizo el estilo del checkout en my orders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,4 +1,4 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 
 const OrdersCart = props => {
@@ -6,13 +6,17 @@ const OrdersCart = props => {
     const { totalPrice, totalProducts } = props
 
     return (
-        <div className="flex justify-between items-center mb-3 border border-black">
-            <p>
-                <span>02.03.2023</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-            </p>
-            <p></p>
+        <div className="flex justify-between items-center border border-black p-4 w-80 rounded-lg mb-4">
+            <div className="flex justify-between w-full">
+                <p className="flex flex-col">
+                    <span className="font-light">02.03.2023</span>
+                    <span className="font-light">{totalProducts} Articles</span>
+                </p>
+                <p className="flex items-center gap-2">
+                    <span className="font-medium text-2xl">${totalPrice}</span>
+                    <ChevronRightIcon className="h-6 w-6 text-black cursor-pointer" />
+                </p>
+            </div>            
         </div>
     );
 
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -11,8 +11,8 @@ function MyOrders() {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center relative w-80">
-        <h1>My Orders</h1>
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl">My Orders</h1>
       </div>
       {
         // todas las props que le enviamos a OrderCard llegan aca directamente
